4670ae0d59196c8cff908392aeaad51c39035b0b
LewisRobertNakhungu <lokiyule@gmail.com>
2025-04-26 22:01:11 +0300

    fix categoryfilter.js

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,47 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 
-function NewTaskForm({ categories, onTaskFormSubmit }) {
-  const [taskText, setTaskText] = useState("");
-  const [taskCategory, setTaskCategory] = useState(categories[0]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newTask = {
-      id: Date.now(),
-      text: taskText,
-      category: taskCategory,
-    };
-    onTaskFormSubmit(newTask);
-    setTaskText("");
-    setTaskCategory(categories[0]);
-  };
-
+function CategoryFilter({
+  categories,
+  selectedCategory,
+  onCategoryChange,
+}) {
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Details
-        <input
-          type="text"
-          value={taskText}
-          onChange={(e) => setTaskText(e.target.value)}
-        />
-      </label>
-      <label>
-        Category
-        <select
-          value={taskCategory}
-          onChange={(e) => setTaskCategory(e.target.value)}
+    <div>
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={category === selectedCategory ? "selected" : ""}
+          onClick={() => onCategoryChange(category)}
         >
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </label>
-      <button type="submit">Add task</button>
-    </form>
+          {category}
+        </button>
+      ))}
+    </div>
   );
 }
 
-export default NewTaskForm;
+export default CategoryFilter;
